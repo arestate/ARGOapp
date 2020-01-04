ebac4a6d5ab6d9cfdb6a2e6307ae1b485c42738d
arestate <argoestate52@gmail.com>
2020-01-04 11:03:47 +0530

    Committed Property details and layout

---
+++ app/src/main/java/com/example/ar_go/PropertyDetails.java
@@ -0,0 +1,14 @@
+package com.example.ar_go;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PropertyDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_property_details);
+    }
+}
