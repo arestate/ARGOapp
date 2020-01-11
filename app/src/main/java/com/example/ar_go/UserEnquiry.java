407ab2d7a5c55941fa931aea4881443d9b05f018
arestate <argoestate52@gmail.com>
2020-01-11 11:59:07 +0530

    Committed Property details and layout

---
+++ app/src/main/java/com/example/ar_go/UserEnquiry.java
@@ -0,0 +1,14 @@
+package com.example.ar_go;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserEnquiry extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_enquiry);
+    }
+}
