f3299f29d3fc9fafcb582f9f14c2b40a53dd1b31
Tamanna Nepali <tamannanepali3144@gmail.com>
2020-01-12 13:49:47 +0530

    AddProperty(Tamanna)

---
+++ app/src/main/java/com/example/ar_go/AddProperty.java
@@ -0,0 +1,14 @@
+package com.example.ar_go;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddProperty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_property);
+    }
+}
