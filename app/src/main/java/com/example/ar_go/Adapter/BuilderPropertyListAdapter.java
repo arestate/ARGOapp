b4e0519231faee8aef0778801a52ceb1aa389bcf
argoestate <argoestate52@gmail.com>
2020-02-14 12:29:46 +0530

    Home fragment commit

---
+++ app/src/main/java/com/example/ar_go/Adapter/BuilderPropertyListAdapter.java
@@ -0,0 +1,68 @@
+package com.example.ar_go.Adapter;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.ar_go.Models.PropertyResultVo;
+import com.example.ar_go.R;
+
+import java.util.List;
+
+
+public class BuilderPropertyListAdapter extends RecyclerView.Adapter<BuilderPropertyListAdapter.ViewHolder>{
+    private List<PropertyResultVo> listdata;
+
+
+
+
+   // RecyclerView recyclerView;
+    public BuilderPropertyListAdapter(List<PropertyResultVo> listdata) {
+        this.listdata = listdata;  
+    }  
+    @Override  
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater layoutInflater = LayoutInflater.from(parent.getContext());  
+        View listItem= layoutInflater.inflate(R.layout.layout_property_row, parent, false);
+        ViewHolder viewHolder = new ViewHolder(listItem);  
+        return viewHolder;  
+    }
+
+
+    @Override  
+    public void onBindViewHolder(ViewHolder holder, int position) {  
+
+        PropertyResultVo propertyResultVo = listdata.get(position);
+
+        holder.tvaddress.setText(propertyResultVo.getPAddress());
+        holder.tvdimensions.setText(propertyResultVo.getPDimensions());
+        holder.aptname.setText(propertyResultVo.getPName());
+
+
+    }  
+  
+  
+    @Override  
+    public int getItemCount() {  
+        return listdata.size();
+    }
+
+
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {  
+
+        TextView aptname,tvaddress,tvdimensions;
+
+
+        public ViewHolder(View itemView) {  
+            super(itemView);  
+
+            aptname=(TextView)itemView.findViewById(R.id.Aptname);
+            tvaddress = (TextView)itemView.findViewById(R.id.tvaddress);
+            tvdimensions = (TextView)itemView.findViewById(R.id.tvdimensions);
+        }  
+    }  
+}  
--- app/src/main/java/com/example/ar_go/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/ar_go/ui/home/HomeFragment.java
@@ -5,31 +5,82 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.ar_go.Adapter.BuilderPropertyListAdapter;
+import com.example.ar_go.Adapter.MyListAdapter;
+import com.example.ar_go.Models.PropertyinfoVo;
 import com.example.ar_go.R;
+import com.example.ar_go.utils.Constants;
+import com.example.ar_go.utils.DataInterface;
+import com.example.ar_go.utils.Webservice_Volley;
+import com.google.gson.Gson;
 
-public class HomeFragment extends Fragment {
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
+public class HomeFragment extends Fragment implements DataInterface {
 
     private HomeViewModel homeViewModel;
+    RecyclerView recvBuilderhome;
+    Webservice_Volley volley;
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
                 ViewModelProviders.of(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+
+        recvBuilderhome = (RecyclerView)root.findViewById(R.id.recvBuilderhome);
+        volley = new Webservice_Volley(getActivity(), this);
+
+        HashMap<String, String> params = new HashMap<>();
+        params.put("b_id","2");
+
+        String url = Constants.Webserive_Url + "get_property_based_on_builders.php";
+
+        volley.CallVolley(url, params, "get_property_based_on_builders");
         return root;
+    }
+
+    @Override
+    public void getData(JSONObject jsonObject, String tag) {
+
+        try {
+
+            Toast.makeText(getActivity(), jsonObject.toString(), Toast.LENGTH_SHORT).show();
+
+            PropertyinfoVo propertyinfoVo = new Gson().fromJson(jsonObject.toString(),PropertyinfoVo.class);
+
+            if (propertyinfoVo != null) {
+
+                if (propertyinfoVo.getResult() != null) {
+
+                    if (propertyinfoVo.getResult().size() > 0) {
+
+                        BuilderPropertyListAdapter adapter = new BuilderPropertyListAdapter(propertyinfoVo.getResult());
+                        recvBuilderhome.setAdapter(adapter);
+
+                    }
+
+                }
+
+            }
+
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+
     }
 }
